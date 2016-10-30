82d9bbe5f17341ce113d9db9ea9df4acd968bcdf
Miller Gonzalez <miller.gonzalez@salesforce.com>
2016-10-30 13:29:32 -0500

    Basic Project template to work on, include LESS, JS minification, CSS minification, watch task and browser sync

---
+++ app/js/main.js
@@ -0,0 +1,3 @@
+console.log('test');
+console.log('test2');
+var saludo = 1;
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Required files
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var path = require('path');
+var concat = require('gulp-concat');
+var uglifycss = require('gulp-uglifycss');
+var plumber = require('gulp-plumber');
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+  autoprefix= new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
+
+var browserSync = require('browser-sync');
+var reload = browserSync.reload
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Script task
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('scripts', function(){
+  //source all JS files contained in app/js/.. folders
+  //More info glob patterns
+  gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('less', function(){
+  gulp.src('app/less/**/*.less')
+    //plumber helps to mantain working gulp even if occur any error on LESS declarations,
+    //Seems only works for SASS because LESS have already implemeted this feature
+    //.pipe(plumber())
+
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+
+    .pipe(concat('allmin.css'))
+
+    //Minify all less
+    /*.pipe(uglifycss({
+        "maxLineLen": 80,
+        "uglyComments": true
+      })
+    )*/
+
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({stream:true}));
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Script task
+//////////////////////////////////////////////////////////////////////////////////////////////
+gulp.task('watch', function(){
+  gulp.watch('app/js/**/*.js', ['scripts']);
+  gulp.watch('app/less/**/*.less', ['less']);
+  gulp.watch('app/**/*.html', ['html']);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// HTML task
+//////////////////////////////////////////////////////////////////////////////////////////////
+gulp.task('html', function(){
+  gulp.src('app/**/*.html')
+  .pipe(reload({stream:true}));
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Browser sync task
+//////////////////////////////////////////////////////////////////////////////////////////////
+gulp.task('browser-sync', function(){
+  browserSync({
+    server: {
+      baseDir: './app/'
+    }
+  })
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Default task
+//////////////////////////////////////////////////////////////////////////////////////////////
+gulp.task('default', ['scripts','less','html','browser-sync','watch']);
