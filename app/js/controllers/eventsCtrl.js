b0bbd5011c031951288465854d9c8ce79c74767c
Miller Gonzalez <miller.gonzalez@salesforce.com>
2016-11-06 21:11:27 -0500

    Create basic event list from HTTP service

--- app/js/controllers/eventsCtrl.js
+++ app/js/controllers/eventsCtrl.js
@@ -6,10 +6,10 @@
 cityApp.controller('eventsCtrl',
   function($scope, $http){
     $scope.events = [];
+    $scope.size = 0;
+
     $http.get('http://citysdk.dmci.hva.nl/CitySDK/events/search?category=festival').success(function(data) {
-      $scope.events = data;
+      $scope.events = data.event;
+      console.log($scope.events);
     });
-
-
-
   })
